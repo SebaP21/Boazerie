8b3dcd10c5ba2241ddd07db1e37981ec43e624d6
Maksymilian Rogacewicz <maksymilian.rogacewicz@gmail.com>
2024-09-10 19:22:16 +0200

    going towards server side

--- src/Components/home/HomeHeader/HomeHeader.tsx
+++ src/Components/home/HomeHeader/HomeHeader.tsx
@@ -1,47 +1,38 @@
-"use client";
-
 import Link from "next/link";
 import styles from "./homeHeader.module.css";
-import { useQuery } from "@apollo/client";
 import { PAGE_HEADER_CONTENT } from "@/graphql/PageHeaderContent";
+import client from "../../../../lib/apolloClient";
 import { PageHeaderQuery } from "../../../../lib/generated/graphql";
 
-export const HomeHeader = () => {
-	const pageId = 36;
+export const HomeHeader = async () => {
+  const { data } = await client.query<PageHeaderQuery>({
+    query: PAGE_HEADER_CONTENT,
+    variables: { pageId: 36 },
+  });
 
-	const { loading, error, data } = useQuery<PageHeaderQuery>(
-		PAGE_HEADER_CONTENT,
-		{
-			variables: { pageId },
-		}
-	);
+  const imageUrl =
+    data?.pageBy?.pageHeading?.headerImageSrc?.node?.mediaItemUrl;
 
-	if (loading) return <p>Ładowanie treści...</p>;
-	if (error) return <p>Błąd: {error.message}</p>;
-
-	const imageUrl =
-		data?.pageBy?.pageHeading?.headerImageSrc?.node?.mediaItemUrl;
-
-	return (
-		<>
-			{data && (
-				<header
-					className={styles.section}
-					style={{ backgroundImage: imageUrl ? `url(${imageUrl})` : "none" }}
-				>
-					<div className={styles.titleBox}>
-						<div className={styles.title}>
-							<h1>{data.pageBy?.pageHeading?.pageHeader}</h1>
-						</div>
-						<div className={styles.titleButton}>
-							<button>
-								<Link href={"#about"}>Sprawdź</Link>
-							</button>
-						</div>
-					</div>
-					<div className={styles.sectionShadow}></div>
-				</header>
-			)}
-		</>
-	);
+  return (
+    <>
+      {data && (
+        <header
+          className={styles.section}
+          style={{ backgroundImage: imageUrl ? `url(${imageUrl})` : "none" }}
+        >
+          <div className={styles.titleBox}>
+            <div className={styles.title}>
+              <h1>{data.pageBy?.pageHeading?.pageHeader}</h1>
+            </div>
+            <div className={styles.titleButton}>
+              <button>
+                <Link href={"#about"}>Sprawdź</Link>
+              </button>
+            </div>
+          </div>
+          <div className={styles.sectionShadow}></div>
+        </header>
+      )}
+    </>
+  );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
-import { Faq } from "@/Components/home/FAQ/Faq";
-import  {HomeHeader}  from "@/Components/home/HomeHeader/HomeHeader";
-import { Offer } from "@/Components/home/Offer/Offer";
+import { HomeHeader } from "@/Components/home/HomeHeader/HomeHeader";
 import { ProductDescription } from "@/Components/home/ProductDescription/ProductDescription";
-
+import { Offer } from "@/Components/home/Offer/Offer";
+import { Faq } from "@/Components/home/FAQ/Faq";
 
 export default function Home() {
-	return (
-		<section>
-			<HomeHeader />
-			<ProductDescription />
-			<Offer />
-			<Faq />
-		</section>
-	);
+  return (
+    <section>
+      <HomeHeader />
+      <ProductDescription />
+      <Offer />
+      <Faq />
+    </section>
+  );
 }
-
-
