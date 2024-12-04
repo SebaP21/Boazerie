47f2b46d1c2cecdca1efc228b36d23ac0c7c1444
Sebastian <sebastianporttka@gmail.com>
2024-12-04 14:00:01 +0100

    decoration p text shadow test

--- src/Components/Portfolio/Products/ProductOne/ProductOne.tsx
+++ src/Components/Portfolio/Products/ProductOne/ProductOne.tsx
@@ -24,15 +24,17 @@ const ProductOne = async () => {
 	return (
 		<article className='w-full flex justify-center'>
 			<div className='w-[95%] flex flex-col gap-6'>
-				<div className='w-full flex justify-center items-center text-black opacity-10 uppercase text-5xl font-extrabold pt-12 pb-4 '>
-					<p className='w-full text-center tracking-widest '>Płycinowa</p>
+				<div className='w-full flex justify-center items-center text-black opacity-30 uppercase text-5xl font-extrabold pt-12 pb-4 '>
+					<p className='w-full text-center tracking-widest text-outline text-white '>
+						Płycinowa
+					</p>
 				</div>
 				<div className='w-full flex flex-col gap-6 justify-center items-center'>
 					<div className='border-b-2 border-accent w-[80%]'>
 						<h3 className='text-3xl text-center mb-4'>{title}</h3>
 					</div>
 					<div>
-						<p className="text-center">{description}</p>
+						<p className='text-center'>{description}</p>
 					</div>
 				</div>
 				<div className=' '>
