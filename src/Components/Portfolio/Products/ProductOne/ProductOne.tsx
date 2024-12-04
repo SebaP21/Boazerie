0a116ce935e99739f39fff5f8fdb6721409f2a8b
Sebastian <sebastianporttka@gmail.com>
2024-12-04 13:47:46 +0100

    tailwind for productOne test

--- src/Components/Portfolio/Products/ProductOne/ProductOne.tsx
+++ src/Components/Portfolio/Products/ProductOne/ProductOne.tsx
@@ -3,6 +3,7 @@ import client from "../../../../../lib/apolloClient";
 import { PortfolioContentTwoQuery } from "../../../../../lib/generated/graphql";
 import styles from "./product_one.module.css";
 import LightboxGallery from "@/Components/Portfolio/Products/Lightbox/Lightbox";
+import Divider from "@/Components/Divider/Divider";
 
 const ProductOne = async () => {
 	const { data } = await client.query<PortfolioContentTwoQuery>({
@@ -21,26 +22,28 @@ const ProductOne = async () => {
 	}));
 
 	return (
-		<article className={styles.productContent}>
-			<div className={styles.sectionTitle}>
-				<p>Płycinowa</p>
-			</div>
-			<div className={styles.description}>
-				<div className={styles.descriptionTitle}>
-					<h3>{title}</h3>
+		<article className='w-full flex justify-center'>
+			<div className='w-[95%] flex flex-col gap-6'>
+				<div className='w-full flex justify-center items-center text-black opacity-10 uppercase text-5xl font-extrabold pt-12 pb-4 '>
+					<p className='w-full text-center tracking-widest '>Płycinowa</p>
 				</div>
-				<div className={styles.descriptionDetails}>
-					<p>{description}</p>
+				<div className='w-full flex flex-col gap-6 justify-center items-center'>
+					<div className='border-b-2 border-accent w-[80%]'>
+						<h3 className='text-3xl text-center mb-4'>{title}</h3>
+					</div>
+					<div>
+						<p className="text-center">{description}</p>
+					</div>
 				</div>
-			</div>
-			<div className=' '>
-				{lightboxImages && (
-					<LightboxGallery
-						images={lightboxImages}
-						divStyle=' w-full grid grid-cols-2 gap-2 lg:grid-cols-3'
-						imgStyle='min-w-[100%] min-h-[100%] object-cover cursor-zoom-in hover:scale-110 transition-all'
-					/>
-				)}
+				<div className=' '>
+					{lightboxImages && (
+						<LightboxGallery
+							images={lightboxImages}
+							divStyle=' w-full grid grid-cols-2 gap-2 lg:grid-cols-3'
+							imgStyle='min-w-[100%] min-h-[100%] object-cover cursor-zoom-in hover:scale-110 transition-all'
+						/>
+					)}
+				</div>
 			</div>
 		</article>
 	);
