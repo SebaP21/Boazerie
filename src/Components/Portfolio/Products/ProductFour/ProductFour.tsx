df01dec3a3fced78b1c32396a621c9b7be99acbe
Sebastian <sebastianporttka@gmail.com>
2024-12-04 11:16:34 +0100

    add molding lightbox

--- src/Components/Portfolio/Products/ProductFour/ProductFour.tsx
+++ src/Components/Portfolio/Products/ProductFour/ProductFour.tsx
@@ -3,8 +3,8 @@ import client from "../../../../../lib/apolloClient";
 import { PortfolioContentTwoQuery } from "../../../../../lib/generated/graphql";
 
 import styles from "./product_four.module.css";
-import Image from "next/image";
 import Divider from "@/Components/Divider/Divider";
+import LightboxGallery from "../Lightbox/Lightbox";
 
 const ProductFour = async () => {
 	const { data } = await client.query<PortfolioContentTwoQuery>({
@@ -15,30 +15,35 @@ const ProductFour = async () => {
 	const gallery =
 		data.allGaleria?.nodes[0].productDescription?.galeriazdjec?.nodes;
 
+	const lightboxImages = gallery?.map((image) => ({
+		src: image.mediaItemUrl || "",
+		alt: image.title || undefined,
+	}));
+
 	return (
-		<article className={styles.productContent} id="sztukateria">
+		<article
+			className={styles.productContent}
+			id='sztukateria'
+		>
 			<div className={styles.sectionTitle}>
 				<p>Sztukateria</p>
 			</div>
 			<div className={styles.description}>
 				<div className={styles.descriptionDetails}>
 					<h3>{title}</h3>
-					<Divider width={80} marginX="mx-auto"/>
+					<Divider
+						width={80}
+						marginX='mx-auto'
+					/>
 				</div>
 				<div className={styles.imageBox}>
-					{gallery?.map((image) => (
-						<div
-							className={styles.imageItem}
-							key={`${image.slug} + ${image.uri}`}
-						>
-							<Image
-								src={image.mediaItemUrl || ""}
-								alt={image.title || "alt"}
-								width={1200}
-								height={1200}
-							/>
-						</div>
-					))}
+				{lightboxImages && (
+					<LightboxGallery
+						images={lightboxImages}
+						divStyle=' w-full grid grid-cols-2 gap-2 lg:grid-cols-3'
+						imgStyle='min-w-[100%] min-h-[100%] object-cover cursor-zoom-in hover:scale-110 transition-all'
+					/>
+				)}
 				</div>
 			</div>
 		</article>
