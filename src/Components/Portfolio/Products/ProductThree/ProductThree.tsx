ee5c79dbdd5e61977c0c456489ed335a8ed54330
Sebastian <sebastianporttka@gmail.com>
2024-12-04 11:39:53 +0100

    all section portfolio lightbox

--- src/Components/Portfolio/Products/ProductThree/ProductThree.tsx
+++ src/Components/Portfolio/Products/ProductThree/ProductThree.tsx
@@ -5,6 +5,7 @@ import { PortfolioContentTwoQuery } from "../../../../../lib/generated/graphql";
 import styles from "./product_three.module.css";
 import Image from "next/image";
 import Divider from "@/Components/Divider/Divider";
+import LightboxGallery from "../Lightbox/Lightbox";
 
 const ProductThree = async () => {
 	const { data } = await client.query<PortfolioContentTwoQuery>({
@@ -18,31 +19,36 @@ const ProductThree = async () => {
 	const gallery =
 		data.allGaleria?.nodes[3].productDescription?.galeriazdjec?.nodes;
 
+	const lightboxImages = gallery?.map((image) => ({
+		src: image.mediaItemUrl || "",
+		alt: image.title || undefined,
+	}));
+
 	return (
-		<article className={styles.productContent} id="boazeria-panelowa">
+		<article
+			className={styles.productContent}
+			id='boazeria-panelowa'
+		>
 			<div className={styles.sectionTitle}>
 				<p>Panelowa</p>
 			</div>
 			<div className={styles.description}>
 				<div className={styles.imageBox}>
-					{gallery?.map((image) => (
-						<div
-							className={styles.imageItem}
-							key={image.slug}
-						>
-							<Image
-								src={image.mediaItemUrl || ""}
-								alt={image.title || ""}
-								width={600}
-								height={600}
-							/>
-						</div>
-					))}
+					{lightboxImages && (
+						<LightboxGallery
+							images={lightboxImages}
+							divStyle=' w-full grid grid-cols-1 gap-2 '
+							imgStyle='min-w-[100%] min-h-[100%] object-cover cursor-zoom-in hover:scale-110 transition-all'
+						/>
+					)}
 				</div>
 
 				<div className={styles.descriptionDetails}>
-					<h3 >{title}</h3>
-					<Divider width={80} marginX="mx-auto"/>
+					<h3>{title}</h3>
+					<Divider
+						width={80}
+						marginX='mx-auto'
+					/>
 					<p>{description}</p>
 				</div>
 			</div>
--- src/Components/Portfolio/Products/ProductTwo/ProductTwo.tsx
+++ src/Components/Portfolio/Products/ProductTwo/ProductTwo.tsx
@@ -5,6 +5,7 @@ import { PortfolioContentTwoQuery } from "../../../../../lib/generated/graphql";
 import styles from "./product_two.module.css";
 import Image from "next/image";
 import Divider from "@/Components/Divider/Divider";
+import LightboxGallery from "../Lightbox/Lightbox";
 
 const ProductTwo = async () => {
 	const { data } = await client.query<PortfolioContentTwoQuery>({
@@ -17,31 +18,33 @@ const ProductTwo = async () => {
 	const gallery =
 		data.allGaleria?.nodes[4].productDescription?.galeriazdjec?.nodes;
 
+	const lightboxImages = gallery?.map((image) => ({
+		src: image.mediaItemUrl || "",
+		alt: image.title || undefined,
+	}));
+
 	return (
-		<article className={styles.productContent} id="boazeria-kasetonowa">
+		<article
+			className={styles.productContent}
+			id='boazeria-kasetonowa'
+		>
 			<div className={styles.sectionTitle}>
 				<p>Kasetonowa</p>
 			</div>
 			<div className={styles.description}>
 				<div className={styles.descriptionDetails}>
 					<h3>{title}</h3>
-					<Divider width={80}/>
+					<Divider width={80} />
 					<p>{description}</p>
 				</div>
 				<div className={styles.imageBox}>
-					{gallery?.map((image) => (
-						<div
-							className={styles.imageItem}
-							key={image.slug}
-						>
-							<Image
-								src={image.mediaItemUrl || ""}
-								alt={image.title || "alt"}
-								width={1000}
-								height={1000}
-							/>
-						</div>
-					))}
+					{lightboxImages && (
+						<LightboxGallery
+							images={lightboxImages}
+							divStyle=' w-full grid grid-cols-2 gap-2 lg:grid-cols-3 xl:grid-cols-4 min-h-[500px]'
+							imgStyle='min-w-[100%] min-h-[100%] object-cover cursor-zoom-in hover:scale-110 transition-all'
+						/>
+					)}
 				</div>
 			</div>
 		</article>
